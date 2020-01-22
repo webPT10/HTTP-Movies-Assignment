704be65389ed2784bfd6e394e2318862021757ee
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-01-21 17:40:18 -0800

    imported useState, set up State [movie, setMovie]

--- client/src/Forms/UpdateMovieForm.js
+++ client/src/Forms/UpdateMovieForm.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function UpdateMovieForm() {
+    const [movie, setMovie] = useState({
+        title: '',
+        director: '',
+        metascore: '',
+        stars: ''
+    })
+
     return(
         <form>
-            
+            <input name='title' />
+            <input name='director' />
+            <input name='metascore' /> 
+            <input name='stars' /> 
         </form>
     )
 }
